7586dbcc865cc305877c5bd33c37d3d975061dd5
thengululo <mthengululo@gmail.com>
2023-06-20 18:57:30 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,16 +7,16 @@ void jack_bauer(void)
 {
 int i, j;
 i = 0;
-while (i < 24)
+while (i < 23)
 {
 j = 0;
-while (j < 60)
+while (j < 59)
 {
-_putchar((i / 10) + '0');
-_putchar((i % 10) + '0');
+_putchar((i / 10) + 48);
+_putchar((i % 10) + 48);
 _putchar(':');
-_putchar((j / 10) + '0');
-_putchar((j % 10) + '0');
+_putchar((j / 10) + 48);
+_putchar((j % 10) + 48);
 _putchar('\n');
 j++;
 }
